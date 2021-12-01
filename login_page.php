93b44b9e2a0db4a613f6b2fb1a3c2a3f3f7b0133
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-01 13:43:17 +0800

    Fix button

--- login_page.php
+++ login_page.php
@@ -1,29 +1,35 @@
- <!DOCTYPE html PUBLIC >
-<html>
-<head>
-	<title>Log In</title>
-	<link rel="stylesheet" href="./includes/style.css">
-</head>
-<body>
+ <!DOCTYPE html PUBLIC>
+ <html>
 
- <?php
-// Print any error messages, if they exist:
-if (isset($errors) && !empty($errors)) {
- echo '<h1>Error!</h1>
+ <head>
+   <title>Log In</title>
+   <link rel="stylesheet" href="./includes/style.css">
+ </head>
+
+ <body>
+
+   <?php
+    // Print any error messages, if they exist:
+    if (isset($errors) && !empty($errors)) {
+      echo '<h1>Error!</h1>
  <p class="error">The following error(s) occurred:<br />';
- foreach ($errors as $msg) {
- echo " - $msg<br />\n";
- }
- echo '</p><p>Please try again.</p>';
-}
+      foreach ($errors as $msg) {
+        echo " - $msg<br />\n";
+      }
+      echo '</p><p>Please try again.</p>';
+    }
 
-?>
- <button><a href = "Create2.php"> <h4>Register</h4>  </a></button>
- <form class="box" action="login.php" method="POST">
-  <h1>Login</h1>
-  <p>Email Address: <input type="text" name="Email" placeholder="Username"></p>
-  <p>Password: <input type="password" name="Password" placeholder="Password"></p>
-  <input type="submit" name="submit" value="Login">
-</form>
-</body>
-</html>
+    ?>
+
+   <form class="box" action="login.php" method="POST">
+     <h1>Login</h1>
+     <p>Email Address: <input type="text" name="Email" placeholder="Username"></p>
+     <p>Password: <input type="password" name="Password" placeholder="Password"></p>
+     <input type="submit" name="submit" value="Login">
+     <button><a href="Create2.php">
+         <h4>Register</h4>
+       </a></button>
+   </form>
+ </body>
+
+ </html>
