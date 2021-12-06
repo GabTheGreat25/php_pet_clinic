7d6d861eb851c30ac56d6ab3d2e84b8ecb59b95b
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-06 11:34:24 +0800

    Center consulationz

--- consultationz.php
+++ consultationz.php
@@ -1,114 +1,128 @@
 <?php
-   session_start(); 
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title>	
+    <title>
         Consultation
-	</title>
-      <meta charset="UTF-8">
+    </title>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/style.css">
-    
+
 </head>
-<body style = "background: url(https://wallpapercave.com/wp/B1sODrM.jpg); background-size: 115% ; ">
+
+<body style="background: url(https://wallpapercave.com/wp/B1sODrM.jpg); background-size: 115% ; ">
     <header>
         <div class="logo">
             <h1>Pet Clinic</h1>
         </div>
-        <button><a href = "consulting.php"> <h4>Consult Your Pet</h4>  </a></button>
+        <button><a href="consulting.php">
+                <h4>Consult Your Pet</h4>
+            </a></button>
         <nav>
             <ul>
-                <button> <a href = "index.php"><h4>Home</h4></a></button>
-                <button> <a href = "pets.php"><h4>Pets</h4></a></button>
-                <button><a href = "customer.php"><h4>Customers</h4></a></button>
-                <button><a href = "employee.php"><h4>Employee</h4></a></button>
-                <button><a href = "service.php"><h4>Service</h4></a></button>
-                <button><a href = "consultationz.php"><h4>consultation</h4></a></button>
+                <button> <a href="index.php">
+                        <h4>Home</h4>
+                    </a></button>
+                <button> <a href="pets.php">
+                        <h4>Pets</h4>
+                    </a></button>
+                <button><a href="customer.php">
+                        <h4>Customers</h4>
+                    </a></button>
+                <button><a href="employee.php">
+                        <h4>Employee</h4>
+                    </a></button>
+                <button><a href="service.php">
+                        <h4>Service</h4>
+                    </a></button>
+                <button><a href="consultationz.php">
+                        <h4>consultation</h4>
+                    </a></button>
             </ul>
         </nav>
-        <button><?php if ( (isset($_SESSION['Employee_id'])) && (basename($_SERVER['PHP_SELF']) != 'logout.php') ) {
-echo '<a href="logout.php"><h3>Logout</h3></a>';
-} else {
-echo '<a href="login.php"><h3>Login</h3></a>';
-}
-?>
-</button>
+        <button><?php if ((isset($_SESSION['Employee_id'])) && (basename($_SERVER['PHP_SELF']) != 'logout.php')) {
+                    echo '<a href="logout.php"><h3>Logout</h3></a>';
+                } else {
+                    echo '<a href="login.php"><h3>Login</h3></a>';
+                }
+                ?>
+        </button>
     </header>
 
-<?php
-error_reporting(0);
-include "includes/config.php";
-if (!isset($_SESSION['Employee_id'])){
-    require ('includes/login_functions.inc.php');
- echo "<p>please log in to check consultations.</p>";
- //echo "<td align='center'><a href='index.php' role='button'> <font color='brightgreek'><h2>Go Back</h2></font></a></td>";
-}
-elseif(isset($_POST['consultationz']))
-{
-    // id to search
-    $Consultation_id  = $_POST['Consultation_id'];
-    
-    // mysql search query
-    $query = "select p.Pet_id,p.Name,c.Consultation_id,c.Date_of_Consultation,c.Disease_Injuries,c.Comments from pet p INNER JOIN consultation c ON p.Pet_id = c.Pet_id WHERE c.Consultation_id = $Consultation_id LIMIT 1";
-    
-    $result = mysqli_query($conn, $query);
-    
-    // if id exist 
-    // show data in inputs
-    if(mysqli_num_rows($result) > 0)
-    {
-      while ($row = mysqli_fetch_array($result))
-      {
-        $Name = $row['Name'];
-        $Date_of_Consultation = $row['Date_of_Consultation'];
-        $Disease_Injuries = $row['Disease_Injuries'];
-        $Comments = $row['Comments'];
-      }  
-    }
-    
-    // if the id not exist
-    // show a message and clear inputs
-    else {
+    <?php
+    error_reporting(0);
+    include "includes/config.php";
+    if (!isset($_SESSION['Employee_id'])) {
+        require('includes/login_functions.inc.php');
+        echo "<p>please log in to check consultations.</p>";
+        //echo "<td align='center'><a href='index.php' role='button'> <font color='brightgreek'><h2>Go Back</h2></font></a></td>";
+    } elseif (isset($_POST['consultationz'])) {
+        // id to search
+        $Consultation_id  = $_POST['Consultation_id'];
+
+        // mysql search query
+        $query = "select p.Pet_id,p.Name,c.Consultation_id,c.Date_of_Consultation,c.Disease_Injuries,c.Comments from pet p INNER JOIN consultation c ON p.Pet_id = c.Pet_id WHERE c.Consultation_id = $Consultation_id LIMIT 1";
+
+        $result = mysqli_query($conn, $query);
+
+        // if id exist 
+        // show data in inputs
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                $Name = $row['Name'];
+                $Date_of_Consultation = $row['Date_of_Consultation'];
+                $Disease_Injuries = $row['Disease_Injuries'];
+                $Comments = $row['Comments'];
+            }
+        }
+
+        // if the id not exist
+        // show a message and clear inputs
+        else {
             $Name = "No Data";
             $Date_of_Consultation = "No Data";
             $Disease_Injuries = "No Data";
             $Comments = "No Data";
+        }
+
+
+        mysqli_free_result($result);
+        mysqli_close($conn);
     }
-    
-    
-    mysqli_free_result($result);
-    mysqli_close($conn);
-    
-}
 
-// in the first time inputs are empty
-else{
-    $Name = "";
-    $Date_of_Consultation = "";
-    $Disease_Injuries = "";
-    $Comments = "";
-}
+    // in the first time inputs are empty
+    else {
+        $Name = "";
+        $Date_of_Consultation = "";
+        $Disease_Injuries = "";
+        $Comments = "";
+    }
 
-?>
-    <form action="consultationz.php" method="POST">
-    <div class="tbl-container">
+    ?>
+    <div class="form-container">
+        <form action="consultationz.php" method="POST" class="form">
 
-        Consultation Id:<input type="number" max="100" min="1" name="Consultation_id"  value="<?php echo $Consultation_id;?>"><br><br>
+            Consultation Id:<input type="number" max="100" min="1" name="Consultation_id" value="<?php echo $Consultation_id; ?>"><br><br>
 
-        Name:<input type="text" name="Name" readonly value="<?php echo $Name;?>"><br><br>
+            Name:<input type="text" name="Name" readonly value="<?php echo $Name; ?>"><br><br>
 
-        Date_of_Consultation:<input type="text" name="Date_of_Consultation" readonly value="<?php echo $Date_of_Consultation;?>"><br><br>
+            Date_of_Consultation:<input type="text" name="Date_of_Consultation" readonly value="<?php echo $Date_of_Consultation; ?>"><br><br>
 
-        Disease_Injuries:<input type="text" name="Disease_Injuries" readonly value="<?php echo $Disease_Injuries;?>"><br><br>
+            Disease_Injuries:<input type="text" name="Disease_Injuries" readonly value="<?php echo $Disease_Injuries; ?>"><br><br>
 
-        Comments: <textarea><?php echo $Comments;?></textarea><br><br>
+            Comments: <textarea><?php echo $Comments; ?></textarea><br><br>
 
-        <input type="submit" name="consultationz" value="Find">
+            <input type="submit" name="consultationz" value="Find">
 
-           </form>
-           </div>
+        </form>
+    </div>
+
+    </div>
 </body>
+
 </html>
--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,6 @@
 DEFINE ('DB_USER', 'root');
 DEFINE ('DB_PASSWORD', '');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'mydb_gabz');
+DEFINE ('DB_NAME', 'mydb');
 $conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() ); 
 //echo "Database ", DB_NAME, " is selected.\n";
-?>
