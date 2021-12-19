56092e3fd618a151f1e12ea36b1ae56cea21c056
GabGab <whysoserious092501@gmail.com>
2021-12-19 21:08:47 +0800

    add ending

--- includes/config.php
+++ includes/config.php
@@ -5,4 +5,5 @@ DEFINE('DB_CHARSET', 'utf8');
 DEFINE('DB_HOST', 'localhost');
 DEFINE('DB_NAME', 'mydb_gabz');
 $conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Could not connect to MySQL: ' . mysqli_connect_error()); 
+?>
 
